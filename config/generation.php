27500273350d23e60ba28c7769f06a7d8ad15365
Dan Pock <self@danpock.me>
2021-07-09 13:24:46 -0400

    Add generator code

--- config/generation.php
+++ config/generation.php
@@ -1,8 +1,23 @@
 <?php
 
+$svgNormalization = static function (string $tempFilepath, array $iconSet) {
+    $doc = new DOMDocument();
+    $doc->load($tempFilepath);
+    $svgElement = $doc->getElementsByTagName('svg')[0];
+    $svgElement->removeAttribute('width');
+    $svgElement->removeAttribute('height');
+    $doc->save($tempFilepath);
+
+    $fileLines = file($tempFilepath);
+    array_shift($fileLines);
+    file_put_contents($tempFilepath, $fileLines);
+};
+
 return [
     [
-        'source' => __DIR__.'/../node_modules/heroicons',
+        'source' => __DIR__.'/../lucide/icons',
         'destination' => __DIR__.'/../resources/svg',
+        'after' => $svgNormalization,
+        'safe' => true,
     ],
 ];
