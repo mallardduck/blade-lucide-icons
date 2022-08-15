150df16b18c37283ff70fcfb64f189ea3e1fa8bb
Dan <self@danpock.me>
2022-08-15 10:33:34 -0400

    Add tests and adjust composer values

--- tests/CompilesIconsTest.php
+++ tests/CompilesIconsTest.php
@@ -7,52 +7,31 @@ namespace Tests;
 use MallardDuck\LucideIcons\BladeLucideIconsServiceProvider;
 use BladeUI\Icons\BladeIconsServiceProvider;
 use Orchestra\Testbench\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
 
 class CompilesIconsTest extends TestCase
 {
+    use MatchesSnapshots;
+
     /** @test */
     public function it_compiles_a_single_anonymous_component()
     {
         $result = svg('lucide-activity')->toHtml();
-
-        // Note: the empty class here seems to be a Blade components bug.
-        $expected = <<<SVG
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <polyline points="22 12 18 12 15 21 9 3 6 12 2 12"/>
-</svg>
-SVG;
-
-        $this->assertSame($expected, $result);
+        $this->assertMatchesXmlSnapshot($result);
     }
 
     /** @test */
     public function it_can_add_classes_to_icons()
     {
         $result = svg('lucide-bell', 'w-6 h-6 text-gray-500')->toHtml();
-
-        $expected = <<<SVG
-<svg class="w-6 h-6 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9"/>
-  <path d="M13.73 21a2 2 0 01-3.46 0"/>
-</svg>
-SVG;
-
-        $this->assertSame($expected, $result);
+        $this->assertMatchesXmlSnapshot($result);
     }
 
     /** @test */
     public function it_can_add_styles_to_icons()
     {
         $result = svg('lucide-bell', ['style' => 'color: #555'])->toHtml();
-
-        $expected = <<<SVG
-<svg style="color: #555" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-  <path d="M18 8A6 6 0 006 8c0 7-3 9-3 9h18s-3-2-3-9"/>
-  <path d="M13.73 21a2 2 0 01-3.46 0"/>
-</svg>
-SVG;
-
-        $this->assertSame($expected, $result);
+        $this->assertMatchesXmlSnapshot($result);
     }
 
     protected function getPackageProviders($app)
